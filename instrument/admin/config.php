b3d4cb5bab57b1d6227a3acf055aa96fae191afc
Cyzja <Cyzja@Cyzja-pc>
2015-02-12 05:46:14 -0300

    comit

---
+++ instrument/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://instrument.surovtsev.by/admin/');
+define('HTTP_CATALOG', 'http://instrument.surovtsev.by/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://instrument.surovtsev.by/admin/');
+define('HTTPS_CATALOG', 'http://instrument.surovtsev.by/');
+
+// DIR
+define('DIR_APPLICATION', '/home/cyzja/www/surovtsev/instrument/admin/');
+define('DIR_SYSTEM', '/home/cyzja/www/surovtsev/instrument/system/');
+define('DIR_LANGUAGE', '/home/cyzja/www/surovtsev/instrument/admin/language/');
+define('DIR_TEMPLATE', '/home/cyzja/www/surovtsev/instrument/admin/view/template/');
+define('DIR_CONFIG', '/home/cyzja/www/surovtsev/instrument/system/config/');
+define('DIR_IMAGE', '/home/cyzja/www/surovtsev/instrument/image/');
+define('DIR_CACHE', '/home/cyzja/www/surovtsev/instrument/system/cache/');
+define('DIR_DOWNLOAD', '/home/cyzja/www/surovtsev/instrument/system/download/');
+define('DIR_UPLOAD', '/home/cyzja/www/surovtsev/instrument/system/upload/');
+define('DIR_LOGS', '/home/cyzja/www/surovtsev/instrument/system/logs/');
+define('DIR_MODIFICATION', '/home/cyzja/www/surovtsev/instrument/system/modification/');
+define('DIR_CATALOG', '/home/cyzja/www/surovtsev/instrument/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Cnfkrth13');
+define('DB_DATABASE', 'instrument.surovtsev.by');
+define('DB_PREFIX', 'oc_');
---
+++ instrument/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://instrument.surovtsev.by/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://instrument.surovtsev.by/');
+
+// DIR
+define('DIR_APPLICATION', '/home/cyzja/www/surovtsev/instrument/catalog/');
+define('DIR_SYSTEM', '/home/cyzja/www/surovtsev/instrument/system/');
+define('DIR_LANGUAGE', '/home/cyzja/www/surovtsev/instrument/catalog/language/');
+define('DIR_TEMPLATE', '/home/cyzja/www/surovtsev/instrument/catalog/view/theme/');
+define('DIR_CONFIG', '/home/cyzja/www/surovtsev/instrument/system/config/');
+define('DIR_IMAGE', '/home/cyzja/www/surovtsev/instrument/image/');
+define('DIR_CACHE', '/home/cyzja/www/surovtsev/instrument/system/cache/');
+define('DIR_DOWNLOAD', '/home/cyzja/www/surovtsev/instrument/system/download/');
+define('DIR_UPLOAD', '/home/cyzja/www/surovtsev/instrument/system/upload/');
+define('DIR_MODIFICATION', '/home/cyzja/www/surovtsev/instrument/system/modification/');
+define('DIR_LOGS', '/home/cyzja/www/surovtsev/instrument/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Cnfkrth13');
+define('DB_DATABASE', 'instrument.surovtsev.by');
+define('DB_PREFIX', 'oc_');
